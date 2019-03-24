05beec5e6bb38c3e5dbab20e4a0c790957a73b58
IngvarOldd <lyushinskij.i.a@gmail.com>
2019-03-24 14:17:23 +0300

    complete task 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let counter,
+      zerosCounter = number;
+  for (let i = 2; i <= base; i++) {
+    counter = 0;
+    while (base % i == 0) {
+      counter++;
+      base = Math.floor(base / i);
+    }
+    let zerosNum = 0;
+        num = number;
+    while (Math.floor(num / i) > 0) {
+      zerosNum += Math.floor(num / i);
+      num = Math.floor(num / i);
+    }
+    zerosCounter = Math.min(zerosCounter, Math.floor(zerosNum / counter));
+  }
+  return zerosCounter;
 }
